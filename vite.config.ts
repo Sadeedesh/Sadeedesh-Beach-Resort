c505f94e3f6e4a4017a0c9593fa6bbd1ddc517c4
Sadeedesh <sandeshkavindaya490@gmail.com>
2026-01-12 17:10:32 +0530

    update my web...

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
-    base: "/Sadeedesh-Beach-Resort/"
+    react()
+  ],
+  base: "/Sadeedesh-Beach-Resort/"
 })
